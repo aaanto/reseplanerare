8bb7d3b9aaf37b4153b69098acbab3b4d48db4dc
RyuZebian <ryuzebian@gmail.com>
2014-10-08 15:19:18 +0200

    Made file, added some structs. Yes.

---
+++ graph.c
@@ -0,0 +1,20 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+struct node {
+  char *nodeID;
+  int NoOfEdges;
+  ------
+}
+
+
+  struct {stationLeaveTime
+
+
+  struct edge {
+    Node nextNode;
+    int timeTaken;
+    int busNumber;
+  }
+
+
